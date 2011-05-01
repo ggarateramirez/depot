d354e570a1178e595ccdc779bea9b657b5ad2519
Gonzalo Gárate <gonzalo.garate@ara.es>
2011-05-01 21:39:48 +0200

    hecho hasta catalogo

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ActiveRecord::Base
+
+#Vista por defecto ordenado por título:
+
+  default_scope :order => 'title'
+
+#Validaciones:
+
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {
+      :greater_than_or_equal_to => 0.01
+  }
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+      :with => %r{\.(gif|jpg|png)$}i,
+      :message  => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
